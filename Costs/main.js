831ec29cd67bbf30224da4ad8c56d54e921079d4
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-04-03 19:36:03 -0300

    Construction of project to use in management costs complete. Functionalities in correct resullt.

--- Costs/main.js
+++ Costs/main.js
@@ -1,26 +1,45 @@
-function some() {
-  receivedValue = parseFloat(document.getElementById('received-value').value)
-  let dizy = receivedValue / 10
-
+function managementValues() {
+  let receivedValue = parseFloat(
+    document.getElementById('received-value').value
+  )
+  let tithe = receivedValue / 10
+  let car = parseFloat(document.getElementById('car').value)
+  let rent = parseFloat(document.getElementById('rent').value)
+  let credeCard = parseFloat(document.getElementById('cred-card').value)
+  let nubankCard = parseFloat(document.getElementById('nubank-card').value)
+  let internet = parseFloat(document.getElementById('internet').value)
   let water = parseFloat(document.getElementById('water').value)
   let eletricEnergy = parseFloat(
     document.getElementById('eletric-energy').value
   )
-  let cookGas = parseFloat(document.getElementById('cook-gas').value)
-  let food = parseFloat(document.getElementById('food').value)
-  let fruits = parseFloat(document.getElementById('fruits').value)
+  let phone = parseFloat(document.getElementById('phone').value)
+  let fuel = parseFloat(document.getElementById('fuel').value)
+  let foods = parseFloat(document.getElementById('foods').value)
+  let others = parseFloat(document.getElementById('others').value)
 
-  let paymentResult = water + eletricEnergy + cookGas + food + fruits
-  let afterPayment = receivedValue - (dizy + paymentResult)
+  let paymentResult =
+    car +
+    rent +
+    credeCard +
+    nubankCard +
+    internet +
+    water +
+    eletricEnergy +
+    phone +
+    fuel +
+    foods +
+    others
+
+  let afterPayment = receivedValue - (tithe + paymentResult)
 
   if (paymentResult > 0) {
-    let result = document.getElementById('results')
-    result.style.display = 'block'
-    result.innerHTML = `Payment result R$ ${paymentResult}`
+    let viewResult = document.getElementById('results')
+    viewResult.style.display = 'block'
+    viewResult.innerHTML = `Payment result R$ ${paymentResult}`
 
-    let dizym = document.getElementById('dizy')
-    dizym.style.display = 'block'
-    dizym.innerHTML = `Dizy result R$ ${dizy}`
+    let titheResult = document.getElementById('tithe')
+    titheResult.style.display = 'block'
+    titheResult.innerHTML = `Tithe result R$ ${tithe}`
 
     let payment = document.getElementById('after-payment-result')
     payment.style.display = 'block'
@@ -28,6 +47,6 @@ function some() {
   } else {
     let invalidResult = document.getElementById('results')
     invalidResult.style.display = 'block'
-    invalidResult.innerHTML = `Resultado inválido! \n preencha todos os campos corretamente.`
+    invalidResult.innerHTML = `Invalid result! Please insert the respective values in each field.`
   }
 }
